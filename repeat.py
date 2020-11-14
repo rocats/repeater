a4766eda9a0a0a67c070b90dcafdb615f54f1903
mzz2017 <m@mzz.pub>
2020-11-14 17:12:53 +0800

    fix flood

--- repeat.py
+++ repeat.py
@@ -1,25 +1,28 @@
 import unicodedata
 import sys
 
-tbl = ''.join(list(chr(i) for i in range(sys.maxunicode)
-                   if unicodedata.category(chr(i)).startswith('P')))
+punctuations = ''.join(list(chr(i) for i in range(sys.maxunicode)
+                            if unicodedata.category(chr(i)).startswith('P')))
 
-SLD = "哦噢喔耶啊哇呀哎哟阿啊呃欸哇呀也耶哟欤呕噢呦嘢吧罢呗啵的价家啦来唻嘞哩咧咯啰喽吗嘛嚜么麽哪呢呐否呵哈不兮般则连罗给噻哉呸了"
+SLD = "哦噢喔耶啊哇呀哎哟阿啊呃诶额欸哇呀也耶哟欤呕噢呦嘢吧罢呗啵的价家啦来唻嘞哩咧咯啰喽吗嘛嚜么麽哪呢呐否呵哈不兮般则连罗给噻哉呸了"
 
 
 def strip_punctuation(s: str):
-    return s.strip(tbl)
+    return s.strip(punctuations)
 
 
-def strip_sld(s: str):
-    return s.strip(SLD)
+def rstrip_sld(s: str):
+    return s.rstrip(SLD)
 
 
 def strip(s: str):
-    return s.strip(tbl + SLD)
+    return s.rstrip(punctuations + SLD).lstrip(punctuations)
 
 
 def repeat(update, context):
+    if len(update.message.text) > 20:
+        # do not flood
+        return
     length = len(strip(update.message.text))
     if 1 <= length <= 3:
         # repeat 3 times with "!"
