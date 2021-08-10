dd22b272a83d5ed2bb6b365ff3809f9a1c2f03e6
mzz <m@mzz.pub>
2021-08-10 20:48:34 +0800

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -7,24 +7,6 @@ from telegram import Update
 from telegram.ext.callbackcontext import CallbackContext
 
 
-punctuations = ''.join(list(chr(i) for i in range(sys.maxunicode)
-                            if unicodedata.category(chr(i)).startswith('P')))
-
-SLD = "哦噢喔耶啊哇呀哎哟阿啊呃诶额欸哇呀也耶哟欤呕噢呦嘢吧罢呗啵的价家啦来唻嘞哩咧咯啰喽吗嘛嚜么麽哪呢呐否呵哈不兮般则连罗给噻哉呸了"
-
-
-def strip_punctuation(s: str):
-    return s.strip(punctuations)
-
-
-def rstrip_sld(s: str):
-    return s.rstrip(SLD)
-
-
-def strip(s: str):
-    return s.rstrip(punctuations + SLD).lstrip(punctuations)
-
-
 last_text = defaultdict(str)
 cnt = defaultdict(int)
 repeated = defaultdict(bool)
@@ -37,15 +19,14 @@ def repeat(update: Update, context: CallbackContext):
     if len(update.message.text) > 50:
         # do not flood
         return
-    t = strip(update.message.text)
+    t = update.message.text.strip()
     if '我' in t and '你' in t:
       t = t.replace('你', '他')
       t = t.replace('我', '你')
     elif '我' in t:
       t = t.replace('我', '你')
-    length = len(t)
     chat_id = update.effective_chat.id
-    if 1 <= length <= 30 and (update.message.text.endswith("！") or update.message.text.endswith("!")):
+    if 1 <= len(update.message.text) <= 30 and (update.message.text.endswith("！") or update.message.text.endswith("!")):
         # repeat 3 times with "!"
         repeated[chat_id] = True
         context.bot.send_message(chat_id=chat_id,
