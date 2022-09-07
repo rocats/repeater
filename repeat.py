db3456b6a81c37ad51d57aa0d0ab89a13566375c
cubercsl <2014cais01@gmail.com>
2022-09-07 08:25:32 +0800

    feat: only repeat two different person

--- repeat.py
+++ repeat.py
@@ -11,6 +11,7 @@ from telegram.ext.callbackcontext import CallbackContext
 punctuations = ''.join(list(chr(i) for i in range(sys.maxunicode)
                             if unicodedata.category(chr(i)).startswith('P')))
 last_text = defaultdict(str)
+last_sender = defaultdict(int)
 cnt = defaultdict(int)
 repeated = defaultdict(bool)
 
@@ -19,13 +20,14 @@ def strip_punctuation(s: str):
 
 def repeat(update: Update, context: CallbackContext):
     global last_text, repeated, cnt
-    if update.message is None or update.message.text is None:
+    if update.message is None or update.message.text is None or update.message.from_user is None:
         return
     if len(update.message.text) > 50:
         # do not flood
         return
     t = update.message.text.strip()
     e = update.message.entities
+    f = update.message.from_user.id
     if '我' in t and '你' in t:
       t = t.replace('你', '他').replace('我', '你')
     elif '我' in t:
@@ -36,11 +38,13 @@ def repeat(update: Update, context: CallbackContext):
         repeated[chat_id] = True
         context.bot.send_message(chat_id=chat_id,
                                  text=(strip_punctuation(t) + "！") * 3)
-    elif update.message.text == last_text[chat_id] and cnt[chat_id] >= 1 and not repeated[chat_id]:
+    elif f != last_sender[chat_id] and update.message.text == last_text[chat_id] \
+        and cnt[chat_id] >= 1 and not repeated[chat_id]:
         # repeat as follower
         repeated[chat_id] = True
         context.bot.send_message(chat_id=chat_id,
                                  text=t, entities=e)
+    last_sender[chat_id] = f
     if update.message.text != last_text[chat_id]:
         last_text[chat_id] = update.message.text
         cnt[chat_id] = 1
